05f1a4ff825cbd2998508b68ad42b8621224ee07
MiguelVagula <160486205+MiguelVagula@users.noreply.github.com>
2024-04-11 12:59:21 +0000

    a

--- home.js
+++ home.js
@@ -2,16 +2,17 @@ import React from 'react';
 import { View, Text, Image, StyleSheet, Pressable, StyleSheet } from 'react-native';
 import { NavigationContainer} from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Home from './home';
 import Conteudo from './conteudo';
 import Sobre from './sobre';
 
-
-export default function App({ navigation }) {
+export default function Home({ navigation }) {
   return (
-    <View>
-        <Text></Text>
-        <Text></Text>
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Text style={styles.title}>Guerra do Golfo</Text>
+        <Text>02/08/1990 – 28/02/1991</Text>
+        <Pressable onPress={navigation}>
+          
+        </Pressable>
     </View>
   )
 }
